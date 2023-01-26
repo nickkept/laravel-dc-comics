009582f75739dde7b8a38108fa63a12ccd8f30d5
nickkept <nickit1899@gmail.com>
2023-01-26 18:31:00 +0100

    finished project

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,7 +2,7 @@
 
 <div>
     <h2 class="text-center">Show Page</h2>
-    <div class="text-center d-flex flex-column">
+    <div class="text-center">
         <a href="{{ route('comics.create')}}" class="btn btn-link">Create</a>
         <a href="{{ route('app')}}" class="btn btn-link">Home</a>
     </div>
