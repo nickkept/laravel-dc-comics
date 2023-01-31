015267961beed0bfe1550e279f6e837b92a35c9f
nickkept <nickit1899@gmail.com>
2023-01-31 16:20:04 +0100

    friday 27/1 part  done

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -58,15 +58,19 @@ class ComicController extends Controller
         return view("comics.show");
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function edit($id)
     {
-        //
+        $comic = Comic::find($id);
+
+        if (!$comic) {
+            // Lancio un messaggio d'errore personalizzato
+            abort(406, "Ritenta, sarai più fortunato");
+        }
+        return view("comics.edit", [
+            "comic" => $comic
+        ]);
     }
 
     /**
@@ -78,7 +82,24 @@ class ComicController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = $request->all();
+        if (!key_exists("available", $data)) {
+            $data["available"] =  false;
+        } else {
+            $data["available"] = true;
+        }
+
+        $comic = Product::findOrFail($id);
+        $comic->name = $data["name"];
+        $comic->description = $data["description"];
+        $comic->price = (float) $data["price"];
+        $comic->available = $data["available"];
+        $comic->save();
+        $comic->update($data);
+
+
+        return redirect()->route("comics.show", $comic->id);
+
     }
 
     /**
@@ -89,6 +110,11 @@ class ComicController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comic = Comic::findOrFail($id);
+
+        $comic->delete();
+
+
+        return redirect()->route("comics.index");
     }
 }
--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+
+    //passo questi parametri o meglio chiavi che non devono essere 
+    protected $fillable = ["name", "description", "price", "available", "img"];
 }
